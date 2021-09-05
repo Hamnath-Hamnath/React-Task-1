6605929a26b783720bed92327a20a421c607c9b7
Hamnath <54175484+Hamnath-Hamnath@users.noreply.github.com>
2021-09-05 21:40:51 +0530

    Search Added

--- src/App.js
+++ src/App.js
@@ -4,24 +4,45 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [post, setPost] = useState([]);
+  const [search, setSearch] = useState("");
   const [completed, setCompleted] = useState(0);
   const [notCompleted, setNotCompleted] = useState(0);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/todos#")
+      .get("https://jsonplaceholder.typicode.com/todos/")
       .then((res) => {
         setPost(res.data);
-        console.log("Data Recieved ==>", post);
+        // console.log("Data Recieved ==>", post);
       })
       .catch((err) => {
         console.log("Error occured while fetching");
       });
+    setCompleted(post.filter((item) => item.completed === true).length);
+    setNotCompleted(post.filter((item) => item.completed === false).length);
   }, []);
 
+  useEffect(() => {
+    const SearchResult = post.filter((title) =>
+      title.toLowerCase().includes(search)
+    );
+    setPost(SearchResult);
+  }, [search]);
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
   return (
     <>
       <div className="container mt-5">
+        <div>
+          <input
+            type="search"
+            placeholder="Search"
+            value={search}
+            onChange={handleChange}
+          />
+        </div>
         <div className="row">
           <div className="col-6 text-success"> Completed - {completed}</div>
           <div className="col-6 text-danger">
@@ -36,16 +57,16 @@ function App() {
                 <th>{data}</th>
               ))
             } */}
-              {/* <th scope="col">#</th> */}
+              <th scope="col">#</th>
               <th scope="col">UserId</th>
               <th scope="col">Title</th>
               <th scope="col">Completed</th>
             </tr>
           </thead>
           <tbody>
-            {post.map((data) => (
+            {post.map((data, index) => (
               <tr key={data.id}>
-                {/* <td>{data.id}</td> */}
+                <td>{index + 1}</td>
                 <td>{data.userId}</td>
                 <td>{data.title}</td>
                 <td>
